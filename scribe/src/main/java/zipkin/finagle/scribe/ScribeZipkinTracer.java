d949b2e93e9f9f608ee7b0937622d7248548cc7e
Adrian Cole <acole@pivotal.io>
2017-02-17 11:10:39 +0100

    Updates to latest auto, reducing some boilerplate

--- scribe/src/main/java/zipkin/finagle/scribe/ScribeZipkinTracer.java
+++ scribe/src/main/java/zipkin/finagle/scribe/ScribeZipkinTracer.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016 The OpenZipkin Authors
+ * Copyright 2016-2017 The OpenZipkin Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -87,9 +87,7 @@ public final class ScribeZipkinTracer extends ZipkinTracer {
           .initialSampleRate(ZipkinTracerFlags.initialSampleRate());
     }
 
-    public Builder toBuilder() {
-      return new AutoValue_ScribeZipkinTracer_Config.Builder(this);
-    }
+    public abstract Builder toBuilder();
 
     abstract Name host();
 
